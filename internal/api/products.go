01c1fe900bf35dfa44d02bd8e307331fc72f293d
Phillip <phillip@freitagsrunde.org>
2019-01-20 05:03:32 +0100

    Add get and post requests for product

--- internal/api/products.go
+++ internal/api/products.go
@@ -12,6 +12,9 @@ func ProductRoutes(router *gin.RouterGroup, config k4ever.Config) {
 	products := router.Group("/products/")
 	{
 		getProducts(products, config)
+		getProduct(products, config)
+		createProduct(products, config)
+		getProductImage(products, config)
 	}
 }
 
@@ -19,9 +22,40 @@ func getProducts(router *gin.RouterGroup, config k4ever.Config) {
 	router.GET("", func(c *gin.Context) {
 		var products []models.Product
 		if err := config.DB().Find(&products).Error; err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, products)
+	})
+}
+
+func getProduct(router *gin.RouterGroup, config k4ever.Config) {
+	router.GET(":id/", func(c *gin.Context) {
+		var product models.Product
+		if err := config.DB().Where("id = ?", c.Param("id")).First(&product).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, product)
+	})
+}
+
+func createProduct(router *gin.RouterGroup, config k4ever.Config) {
+	router.POST("", func(c *gin.Context) {
+		var product models.Product
+		if err := c.ShouldBindJSON(&product); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if err := config.DB().Create(&product).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, product)
+	})
+}
+
+func getProductImage(router *gin.RouterGroup, config k4ever.Config) {
+	router.GET(":id/image/", func(c *gin.Context) {
+		c.JSON(http.StatusNotImplemented, gin.H{"Hello": "World"})
 	})
 }
--- internal/models/product.go
+++ internal/models/product.go
@@ -4,7 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Product struct {
 	gorm.Model
-	ProductName        string
+	ProductName        string `json:"name"`
 	ProductPrice       float64
 	ProductDescription string `gorm:"type:text;"`
 	ProductImage       string
