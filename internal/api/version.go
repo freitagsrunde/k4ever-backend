6e8f565a9b60c9edcb0d451914866e5f171f44f5
Phillip <phillip@freitagsrunde.org>
2019-02-28 21:41:17 +0100

    Add swagger annotation for version

--- internal/api/version.go
+++ internal/api/version.go
@@ -14,13 +14,24 @@ func VersionRoutesPublic(router *gin.RouterGroup, config k4ever.Config) {
 	}
 }
 
+// swagger:model
 type VersionInformation struct {
-	Version   string
-	GitBranch string
-	GitCommit string
-	BuildTime string
+	Version   string `json:"version"`
+	GitBranch string `json:"branch"`
+	GitCommit string `json:"commit"`
+	BuildTime string `json:"build_time"`
 }
 
+// swagger:route GET /version/ getVersion
+//
+// Fetch version information
+//
+//		Produces:
+//		- application/json
+//
+//		Responses:
+//		  default: VersionInformation
+//		  200: VersionInformation
 func GetVersion(router *gin.RouterGroup, config k4ever.Config) {
 	version := VersionInformation{Version: config.Version(), GitBranch: config.GitBranch(), GitCommit: config.GitCommit(), BuildTime: config.BuildTime()}
 
