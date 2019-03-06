5bc29a9ca3bed47e322c37afc72b9d0fe4e5d58c
Phillip <phillip@freitagsrunde.org>
2019-03-06 17:23:17 +0100

    Make purchaseitems non unique

--- internal/models/purchase.go
+++ internal/models/purchase.go
@@ -8,10 +8,47 @@ type PurchaseItem struct {
 	Amount int `json:"amount"`
 
 	// Information about the bought product
-	ProductInformation
+	PurchaseItemInformation
 
 	ProductID  uint `json:"product_id"`
 	PurchaseID uint `json:"-"`
+}
+
+type PurchaseItemInformation struct {
+	// The name of the product
+	//
+	// required: true
+	// min: 1
+	//
+	// example: club mate
+	Name string `json:"name" gorm:"not_null"`
+
+	// The price of the product
+	//
+	// required: true
+	//
+	// example: 1.00
+	Price float64 `json:"price" gorm:"not_null"`
+
+	// The description of the product
+	//
+	// required: false
+	Description string `json:"description" gorm:"type:text;"`
+
+	// An optional deposit meant for drinks
+	//
+	// required: false
+	Deposit float64 `json:"deposit"`
+
+	// The barcode of the product
+	//
+	// required: false
+	Barcode string `json:"barcode" gorm:"default: null"`
+
+	// Currently the path to the image (tbi)
+	//
+	// required: false
+	Image string `json:"image"`
 }
 
 // swagger:model
